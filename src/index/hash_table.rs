b51d53f0f683b241b5ae58185b59fbb5ea7099d5
tibvdm <tibovandemoortele@hotmail.com>
2022-04-08 21:12:13 +0200

    will not compile, but implements a logarithmic search with missing error stuff

--- src/index/hash_table.rs
+++ src/index/hash_table.rs
@@ -5,8 +5,12 @@ use crate::hash::fnv_1a_hash::Fnv1aHasher32;
 // TODO: how about we give the ftable ordening to the hashtable, then this list can be updated in place
 // Then later we use this list to write to a csv file in the correct order.
 
-// TODO: no need to update bucket counters, becouse 2D vector
+// TODO: no need to update bucket counters, because 2D vector
 // !!! However, bucket indices have to point to 1D indices, and later fix this in finish();
+
+// TODO: Hasher could be static in this case
+
+// TODO: no need to always update the counts, because we work with 2D arrays
 
 /// TODO
 pub struct HashTable {
@@ -40,6 +44,8 @@ impl HashTable {
         let first_conflict_index = self.buckets[bucket]
 
         // This is the first hit for our bucket
+        // TODO: WILL NEVER BE -1, BECAUSE THIS IS AN UNSIGNED INTEGER 32
+        // SOLUTION: Add 1 extra if that will only be executed once, then branching will get rid of this case.
         if first_conflict_index == -1 {
             // Prepare the entry to be pushed (#conflicts in last 19 bits).
             let entry: u64 = kmer.0 | 0x0000200000000000;
@@ -72,9 +78,40 @@ impl HashTable {
     }
 
     /// TODO
+    pub fn get(&self, kmer: &Kmer) -> Result<u32> {
+        // Calculate the bucket of this k-mer
+        let bucket = self.hasher.hash(kmer);
+
+        // Retrieve the position of the first conflict
+        let first_conflict_index = self.buckets[bucket]
+
+        // Perform a logarithmic search to find the correct entry
+        Ok(log_search(kmer, first_conflict_index))
+    }
+
+    /// TODO
     pub fn finish() {
         // TODO: order is OK, because we start with an ordered list of kmers ()
         // TODO: place the 2D vector sequentieel in memory
         // TODO: maybe final changes voor de ftable order list
+    }
+
+    fn log_search(&self, kmer: &Kmer, first_conflict_index: u32) -> Result<u32> {
+        let lower: u32 = 0;
+        let upper: u32 = len(self.stack[first_conflict_index]) - 1;
+
+        while lower < upper {
+            let middle: u32 = (lower + upper) / 2;
+            if self.stack[first_conflict_index][middle] < kmer.0 {
+                lower = middle + 1;
+            } else if self.stack[first_conflict_index][middle] > kmer.0 {
+                upper = middle - 1;
+            } else {
+                Ok(first_conflict_index + middle)
+            }
+        }
+
+        // TODO throw error
+        Ok(0)
     }
 }
--- src/index/index.rs
+++ src/index/index.rs
@@ -1,3 +1,37 @@
 //! TODO: Combination of the funnel table and the functional table.
 
 // TODO
+
+/// TODO
+pub struct BowtieIndex {
+    /// TODO
+    hash_table: HashTable
+    /// TODO
+    functional_table: FunctionalTable
+}
+
+impl BowtieIndex {
+    /// TODO
+    pub fn new() -> BowtieIndex {
+        // TODO: Build hashtable
+        hash_table: HashTable = HashTable::new();
+
+        // TODO: Build functional table
+        functional_table: FunctionalTable = FunctionalTable::new();
+        
+        BowtieIndex {
+            hash_table: hash_table,
+            functional_table: functional_table
+        }
+    }
+
+    /// TODO
+    pub fn search(&self, kmer: &Kmer) -> u32 {
+        // TODO: implement log_search
+    }
+
+    /// TODO
+    fn log_search(&self, kmer: &Kmer) -> u32 {
+
+    }
+}
